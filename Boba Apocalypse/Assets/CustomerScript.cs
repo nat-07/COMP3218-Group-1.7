ae0070b92178079f2e49a6009005b6cabf835086
Nithindree Mendis <64081792+Heather-Mendis@users.noreply.github.com>
2025-10-30 13:30:00 +0000

    NPC basic logic

---
+++ Boba Apocalypse/Assets/CustomerScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomerScript : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public float waitTime = 5f;
+    public GameObject bobaObject;
+    public GameObject timerObject;  
+
+    private enum State { MovingToCenter, Waiting, MovingToExit }
+    private State currentState = State.MovingToCenter;
+    private float waitTimer = 0f;
+
+    void Start()
+    {
+        transform.position = new Vector3(-10f, transform.position.y, transform.position.z);
+
+        //HIDE BOBA 
+        if (bobaObject != null)
+        {
+            bobaObject.SetActive(false);
+        }
+        if (timerObject != null)
+        {
+            timerObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case State.MovingToCenter:
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+                if (transform.position.x >= 0f)
+                {
+                    currentState = State.Waiting;
+                    waitTimer = 0f;
+
+                    // SHow at center
+                    if (bobaObject != null)
+                    {
+                        bobaObject.SetActive(true);
+                    }
+                    if (timerObject != null)
+                    {
+                        timerObject.SetActive(true);
+                    }
+                }
+                break;
+
+            case State.Waiting:
+                waitTimer += Time.deltaTime;
+
+                if (waitTimer >= waitTime)
+                {
+                    currentState = State.MovingToExit;
+
+                   
+                    if (bobaObject != null)
+                    {
+                        bobaObject.SetActive(false);
+                    }
+                    
+                }
+                break;
+
+            case State.MovingToExit:
+                transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+                if (transform.position.x > 10f)
+                {
+                    Destroy(gameObject);
+                }
+                break;
+        }
+    }
+}
---
+++ Boba Apocalypse/Assets/Timer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Experimental.Rendering;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timerText;
+    [SerializeField] float remainingTime;
+
+    void Update()
+    {
+        if (remainingTime > 0)
+        {
+            remainingTime -= Time.deltaTime;
+            int minutes = Mathf.FloorToInt(remainingTime / 60);
+            int seconds = Mathf.FloorToInt(remainingTime % 60);
+            timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+        else
+        {
+            // hide timer when 0
+            gameObject.SetActive(false);
+        }
+    }
+}
