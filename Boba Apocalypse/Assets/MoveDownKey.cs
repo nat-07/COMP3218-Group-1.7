67dddfc27e811f42d982816741efe094be19a949
nat-07 <natashaudani05@gmail.com>
2025-10-30 14:59:46 +0000

    added animation to shooting thing

--- Boba Apocalypse/Assets/MoveDownKey.cs
+++ Boba Apocalypse/Assets/MoveDownKey.cs
@@ -9,15 +9,20 @@ public class MoveDownKey : MonoBehaviour
     public GameObject[] objectsToTrack;
     public GameObject[] otherObjectsToTrack;// assign in Inspector
     private Vector3[] initialPositions;
+    private Vector3[] initialPositions2;
+
     public float distance = 20f;
     public float duration = 2f;
 
     void Start()
 {
     initialPositions = new Vector3[objectsToTrack.Length];
-    for (int i = 0; i < objectsToTrack.Length; i++)
-        initialPositions[i] = objectsToTrack[i].transform.position;
-        
+        for (int i = 0; i < objectsToTrack.Length; i++)
+            initialPositions[i] = objectsToTrack[i].transform.position;
+    initialPositions2 = new Vector3[otherObjectsToTrack.Length];
+    for (int i = 0; i < otherObjectsToTrack.Length; i++)
+        initialPositions2[i] = otherObjectsToTrack[i].transform.position;
+   
 }
 
     void Update()
@@ -25,18 +30,29 @@ public class MoveDownKey : MonoBehaviour
         // Example: press space to move all down relative to initial position
        if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-           for (int i = 0; i < objectsToTrack.Length; i++)
-           {
-            var obj = objectsToTrack[i];
+            for (int i = 0; i < objectsToTrack.Length; i++)
+            {
+                var obj = objectsToTrack[i];
 
                 Vector3 startPos = obj.transform.position; // current pos
-                    Vector3 peakPos = startPos + Vector3.up * 0.2f;
+                Vector3 peakPos = startPos + Vector3.up * 0.2f;
                 Vector3 endPos = initialPositions[i] + Vector3.down * 3; // always relative to start
+                Sequence seq = DOTween.Sequence();
+                seq.Append(obj.transform.DOMove(peakPos, duration * 0.2f).SetEase(Ease.OutQuad));
+                seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
+            }
 
-    Sequence seq = DOTween.Sequence();
-    seq.Append(obj.transform.DOMove(peakPos, duration * 0.2f).SetEase(Ease.OutQuad));
-    seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
-}
+            for (int i = 0; i < otherObjectsToTrack.Length; i++)
+            {
+                var obj = otherObjectsToTrack[i];
+
+                Vector3 startPos = obj.transform.position; // current pos
+                Vector3 peakPos = startPos + Vector3.down * 0.2f;
+                Vector3 endPos = initialPositions2[i] + Vector3.up * 7.2f;
+                Sequence seq = DOTween.Sequence();
+                seq.Append(obj.transform.DOMove(peakPos, duration * 0.2f).SetEase(Ease.OutQuad));
+                seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
+            }
 
         }
     
@@ -44,16 +60,25 @@ public class MoveDownKey : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             for (int i = 0; i < objectsToTrack.Length; i++)
-           {
-            var obj = objectsToTrack[i];
+            {
+                var obj = objectsToTrack[i];
 
                 Vector3 peakPos = initialPositions[i] + Vector3.up * 0.2f;
                 Vector3 endPos = initialPositions[i]; // always relative to start
+                Sequence seq = DOTween.Sequence();
+                seq.Append(obj.transform.DOMove(peakPos, duration).SetEase(Ease.OutQuad));
+                seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
+            }
+            for (int i = 0; i < otherObjectsToTrack.Length; i++)
+           {
+            var obj = otherObjectsToTrack[i];
 
-    Sequence seq = DOTween.Sequence();
-    seq.Append(obj.transform.DOMove(peakPos, duration).SetEase(Ease.OutQuad));
-    seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
-}
+                Vector3 peakPos = initialPositions2[i] + Vector3.down * 0.2f;
+                Vector3 endPos = initialPositions2[i]; // always relative to start
+                Sequence seq = DOTween.Sequence();
+                seq.Append(obj.transform.DOMove(peakPos, duration).SetEase(Ease.OutQuad));
+                seq.Append(obj.transform.DOMove(endPos, duration * 0.8f).SetEase(Ease.InQuad));
+                }
         }
     }
 }
